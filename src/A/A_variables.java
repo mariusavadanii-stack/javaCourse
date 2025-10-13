043dcbabed17f280fd45645da358c2c4bf5572fa
Admin <marius.avadanii@gmail.com>
2025-10-13 12:24:28 +0200

    first

---
+++ src/A/A_variables.java
@@ -0,0 +1,41 @@
+package A;
+
+public class A_variables {
+
+    static void main(String[] args) {
+
+        System.out.println("hello world");
+        System.out.println("ciao");
+
+        // Variables - primitive
+        // INT BYTE SHORT LONG
+        // declaration and setting a value
+         int firstInt;      //declaration
+         firstInt = 18;     //setting value
+
+        int secondInt = 45;
+        System.out.println(secondInt);
+        System.out.println(firstInt);
+
+        //small value like smaller than 100
+        byte firstByte = 78;
+        System.out.println(firstByte);
+
+        // short is a short int long
+        short firstShort = 30333;
+        long firstLong  = 222225033;
+
+        //mix strings with parsed values
+
+        System.out.println("my first " + firstByte +  "and my second" +secondInt);
+
+
+
+
+
+
+
+
+    }
+
+}
